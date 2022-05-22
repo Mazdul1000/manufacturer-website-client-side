1061a105d37fb2783327319d27d447f5ad7e9e90
Mazdul1000 <mazdul1000@gmail.com>
2022-05-22 22:13:35 +0600

    add dashboard section

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import Footer from './pages/Shared/Footer';
 import Headers from './pages/Shared/Headers';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Dashboard from './pages/Dashboard/Dashboard';
+import MyProfile from './pages/Dashboard/MyProfile';
+import AddReview from './pages/Dashboard/AddReview';
+import MyOrders from './pages/Dashboard/MyOrders';
 
 
 
@@ -22,6 +26,11 @@ function App() {
      <Routes>
        <Route path='/' element={<Home></Home>}></Route>
        <Route path='/home' element={<Home></Home>}></Route>
+       <Route path='/dashboard' element={<Dashboard></Dashboard>}>
+         <Route index element={<MyProfile></MyProfile>}></Route>
+         <Route path='/dashboard/addreview' element={<AddReview></AddReview>}></Route>
+         <Route path='/dashboard/myorders' element={<MyOrders></MyOrders>}></Route>
+       </Route>
        <Route path='/login' element={<Login></Login>}></Route>
        <Route path='/signup' element={<SignUp></SignUp>}></Route>
        <Route path='*' element={<NotFound></NotFound>}></Route>
---
+++ src/pages/Dashboard/MyProfile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyProfile = () => {
+    return (
+        <div>
+            <h1 className="text-5xl text-secondary">My Profile</h1>
+        </div>
+    );
+};
+
+export default MyProfile;
