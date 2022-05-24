367e5518caef5804dbb25edcf025462c79ab07d3
Mazdul1000 <mazdul1000@gmail.com>
2022-05-24 18:18:07 +0600

    update add new product page

--- src/pages/Dashboard/AddProduct.js
+++ src/pages/Dashboard/AddProduct.js
@@ -1,9 +1,108 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
 
 const AddProduct = () => {
+    const { register, formState: { errors }, handleSubmit, reset } = useForm();
+
+    const onSubmit = data => {
+
+       
+        const product = {
+            name:data.name,
+            price: data.price,
+            stock: data.stock,
+            minOrder: data.minOrder,
+            img:data.img,
+            description:data.description,
+            rating: data.rating
+        }
+
+        fetch(' http://localhost:5000/product', {
+            method: 'POST',
+            headers: {
+              'content-type': 'application/json'
+            },
+            body: JSON.stringify(product)
+
+          })
+            .then(res => res.json())
+            .then(data => {
+                if(data.insertedId){
+                   reset();
+                toast.success("Product added to the inventory")  
+                }
+                else{
+                    toast.error('Operation Faild')
+                }
+               
+            })
+    };
     return (
         <div>
-            <h1 className="text-5xl">Add a Product</h1>
+            <h1 className="text-5xl text-center my-3 text-primary font-mono font-semibold">Add a Product</h1>
+
+            <div className='flex min-h-[80vh] justify-center items-center'>
+                <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-lg border-2 p-5 rounded-lg border-primary">
+                    <div className="flex flex-wrap -mx-3 mb-6">
+                        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="product-name">
+                                Product Name
+                            </label>
+                            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Product Name" {...register("name", { required: true })} />
+
+                        </div>
+                        <div className="w-full md:w-1/2 px-3">
+                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="price">
+                                Price(per unit)
+                            </label>
+                            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="number" placeholder="Price" {...register("price", { required: true })} />
+                        </div>
+                    </div>
+                    <div className="flex flex-wrap -mx-3 mb-2">
+                        <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="stock">
+                                Stock
+                            </label>
+                            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="stock" type="number" placeholder='Stock' {...register("stock", { required: true })} />
+                        </div>
+
+                        <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="min-order">
+                                Minumum Order
+                            </label>
+                            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="min-order" type="number" {...register("minOrder", { required: true })} />
+                        </div>
+                        <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="rating">
+                                Rating
+                            </label>
+                            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="rating" type="number" placeholder='Rating' {...register("rating", { required: true })} />
+                        </div>
+                    </div>
+
+                    <div className="flex flex-wrap -mx-3 mb-6">
+                        <div className="w-full px-3">
+                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="image">
+                                Image
+                            </label>
+                            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="image" type="text" {...register("img", { required: true })}/>
+                        </div>
+                    </div>
+                    <div className="flex flex-wrap -mx-3 mb-6">
+                        <div className="w-full px-3">
+                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="description">
+                                Description
+                            </label>
+                            <textarea className="appearance-none block h-[100px] w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 resize-none" id="description" type="text" placeholder='Description'  {...register("description", { required: true })} />
+                        </div>
+                    </div>
+                    <div className='flex justify-center'>
+                        <input className='btn w-2/3 btn-primary text-white' type="submit" value='Submit' />
+                    </div>
+
+                </form>
+            </div>
         </div>
     );
 };
